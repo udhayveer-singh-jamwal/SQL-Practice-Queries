2673d4a42135e04ddeec27a3fef56c88c240823b
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-07 22:27:39 +0530

    Update aggregates.sql

--- Basics/aggregates.sql
+++ Basics/aggregates.sql
@@ -1,4 +1,3 @@
--- Simple aggregates on whole table
 SELECT
   COUNT(*)          AS total_employees,
   MIN(salary)       AS min_salary,
@@ -7,19 +6,16 @@ SELECT
   SUM(salary)       AS total_salary
 FROM Employees;
 
--- GROUP BY (basic)
 SELECT dept, COUNT(*) AS headcount
 FROM Employees
 GROUP BY dept
 ORDER BY headcount DESC;
 
--- Aggregate + GROUP BY (average salary per dept)
 SELECT dept, ROUND(AVG(salary), 2) AS avg_salary
 FROM Employees
 GROUP BY dept
 ORDER BY avg_salary DESC;
 
--- HAVING (filter groups after aggregation)
 SELECT dept, COUNT(*) AS cnt, ROUND(AVG(salary),2) AS avg_sal
 FROM Employees
 GROUP BY dept
