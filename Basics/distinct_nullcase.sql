62a3f5ca2eb6927b485ac100b60638b70f351f88
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-07 22:27:13 +0530

    Clean up distinct_nullcase.sql formatting
    Removed comments and unnecessary whitespace in SQL queries.

--- Basics/distinct_nullcase.sql
+++ Basics/distinct_nullcase.sql
@@ -1,11 +1,6 @@
--- DISTINCT with multiple columns
 SELECT DISTINCT dept, manager_id FROM Employees;
-
--- COALESCE to replace NULLs
 SELECT emp_name, COALESCE(bonus_pct, 0) AS bonus_pct_non_null
 FROM Employees;
-
--- CASE expression (label salary bands)
 SELECT
   emp_name,
   salary,
