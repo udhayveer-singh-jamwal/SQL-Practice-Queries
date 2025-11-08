46e0b009afdb9b761f1ad44bc164215caa666598
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-08 22:12:36 +0530

    Update update_delete.sql

--- Basics/update_delete.sql
+++ Basics/update_delete.sql
@@ -4,10 +4,7 @@ WHERE dept = 'Sales';
 UPDATE Employees
 SET bonus_pct = 5.00
 WHERE bonus_pct IS NULL;
-
--- DELETE (remove anyone with salary < 42000)
 DELETE FROM Employees
 WHERE salary < 42000;
 
--- Safe check after update/delete
 SELECT * FROM Employees ORDER BY emp_id;
