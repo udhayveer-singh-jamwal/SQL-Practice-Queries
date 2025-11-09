cc27149135cb4edcbaddffad997cd97d3634f6e2
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-09 22:24:12 +0530

    Create subqueries.sql

---
+++ Intermediate/subqueries.sql
@@ -0,0 +1,26 @@
+-- Subquery: Employees earning more than avg salary
+SELECT emp_name, salary
+FROM Employees
+WHERE salary > (SELECT AVG(salary) FROM Employees);
+
+-- Subquery: Employees from same department as 'Neha'
+SELECT emp_name, dept_id
+FROM Employees
+WHERE dept_id = (
+  SELECT dept_id FROM Employees WHERE emp_name = 'Neha'
+);
+
+-- Subquery in FROM clause
+SELECT dept_id, COUNT(*) AS emp_count, ROUND(AVG(salary),2) AS avg_sal
+FROM Employees
+GROUP BY dept_id
+HAVING AVG(salary) > (
+  SELECT AVG(salary) FROM Employees
+);
+
+-- EXISTS
+SELECT emp_name, dept_id
+FROM Employees e
+WHERE EXISTS (
+  SELECT 1 FROM Departments d WHERE d.dept_id = e.dept_id
+);
