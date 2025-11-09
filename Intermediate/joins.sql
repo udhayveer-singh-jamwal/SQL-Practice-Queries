aded8b404b979f62db4416ea0d688dd0300c92f9
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-09 22:23:10 +0530

    Update joins.sql

--- Intermediate/joins.sql
+++ Intermediate/joins.sql
@@ -1,29 +1,22 @@
--- INNER JOIN
 SELECT e.emp_name, e.salary, d.dept_name, d.location
 FROM Employees e
 INNER JOIN Departments d
 ON e.dept_id = d.dept_id;
 
--- LEFT JOIN (all employees, even if department missing)
 SELECT e.emp_name, d.dept_name
 FROM Employees e
 LEFT JOIN Departments d
 ON e.dept_id = d.dept_id;
 
--- RIGHT JOIN (all departments, even if no employee)
 SELECT e.emp_name, d.dept_name
 FROM Employees e
 RIGHT JOIN Departments d
 ON e.dept_id = d.dept_id;
 
--- SELF JOIN (manager-employee relationship)
 SELECT e.emp_name AS Employee, m.emp_name AS Manager
 FROM Employees e
 LEFT JOIN Employees m
 ON e.manager_id = m.emp_id;
-
--- CROSS JOIN (all combinations)
 SELECT e.emp_name, d.dept_name
 FROM Employees e
 CROSS JOIN Departments d;
-
