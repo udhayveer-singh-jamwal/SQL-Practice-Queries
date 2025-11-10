e43d25de8d522856bb0fed3983f33d2e0b184e33
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-10 22:54:54 +0530

    Update string_functions.sql

--- Intermediate/string_functions.sql
+++ Intermediate/string_functions.sql
@@ -1,12 +1,9 @@
--- Upper / Lower case
 SELECT emp_name, UPPER(emp_name) AS upper_name, LOWER(emp_name) AS lower_name
 FROM Employees;
 
--- Concatenate
 SELECT emp_name, CONCAT(emp_name, ' works in ', dept_id) AS emp_info
 FROM Employees;
 
--- Substring
 SELECT emp_name, SUBSTRING(emp_name FROM 1 FOR 3) AS short_name
 FROM Employees;
 
