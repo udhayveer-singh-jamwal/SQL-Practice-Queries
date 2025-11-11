1f4e0276dc1596d694c70b90b0317a012931c584
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-11 22:26:26 +0530

    Update constraints_keys.sql

--- Intermediate/constraints_keys.sql
+++ Intermediate/constraints_keys.sql
@@ -1,6 +1,4 @@
--- Drop old table if exists
 DROP TABLE IF EXISTS Employees;
-
 CREATE TABLE Employees (
   emp_id INT PRIMARY KEY,
   emp_name VARCHAR(50) NOT NULL,
@@ -10,9 +8,4 @@ CREATE TABLE Employees (
   hire_date DATE DEFAULT CURRENT_DATE,
   FOREIGN KEY (dept_id) REFERENCES Departments(dept_id)
 );
-
--- Insert valid data
 INSERT INTO Employees VALUES (1, 'Amit', 101, 45000, 'amit@company.com', '2021-02-10');
-
--- Check constraint example (will fail)
--- INSERT INTO Employees VALUES (2, 'Sara', 102, -20000, 'sara@company.com', '2022-05-12');
