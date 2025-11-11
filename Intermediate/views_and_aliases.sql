5b3485113ae4320add3cb0742447e5dc4f79ce72
Udhayveer Singh Jamwal <jamwal.udhayveer@gmail.com>
2025-11-11 22:26:48 +0530

    Update views_and_aliases.sql

--- Intermediate/views_and_aliases.sql
+++ Intermediate/views_and_aliases.sql
@@ -1,11 +1,6 @@
--- Create a view for high salary employees
 CREATE OR REPLACE VIEW HighSalary AS
 SELECT emp_name, dept_id, salary
 FROM Employees
 WHERE salary > 60000;
-
--- Use the view
 SELECT * FROM HighSalary;
-
--- Drop view
 DROP VIEW IF EXISTS HighSalary;
